ac4283ec18efe1977f9db21af9adacad5c829d8a
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2012-06-24 16:49:04 +0200

    pickup of missing files

---
+++ generate_interface.pl
@@ -0,0 +1,28 @@
+sub Member {
+    my $name=shift;
+    print "template <class T> void $name (const T &);
+}
+Member( 'record_start_position');
+Member( 'set_action');
+Member( 'set_current_cs');
+Member( 'set_current_element_type_member');
+Member( 'set_current_element_type_nd');
+Member( 'set_current_element_type_node');
+Member( 'set_current_element_type_rel'););
+Member( 'set_current_element_type_tag');
+Member( 'set_current_element_type_way');
+Member( 'set_current_id'););
+Member( 'set_current_lat');
+Member( 'set_current_lon');
+Member( 'set_current_ts_day');
+Member( 'set_current_ts_hour');
+Member( 'set_current_ts_minute');
+Member( 'set_current_ts_month');
+Member( 'set_current_ts_second');
+Member( 'set_current_uid');
+Member( 'set_current_user');
+Member( 'set_current_ver');
+Member( 'set_current_vis');
+Member( 'set_tag_key');
+Member( 'set_tag_val');
+Member( 'set_way_node_ref' );
---
+++ process-fosm2.cpp
@@ -0,0 +1,35 @@
+int process_line(const char * buffer); // null terminated
+//#include <iostream>
+//using namespace std;
+#include "ifileindexer.hpp"
+
+extern OSMWorld iworld; 
+int scanner(OSMWorld & world,const char *s);
+int prescanner(OSMWorld & world,const char *s);
+long int seen=0;
+int process_line(const char * buffer)
+{
+  //  cerr << "BEGIN " << buffer << "ENDL"<< endl;
+  iworld.set_current_pos(seen);
+  size_t len=strlen(buffer);
+  seen +=  len;
+  if (len>2)
+    {
+      int ret=0;
+      //TODO :ret=scanner(iworld,buffer);
+   
+      if (ret != 1)        {          
+          cerr << "scanner returned ret " << ret << " for len :" << strlen(buffer) << endl;
+      }else   {
+        //cerr << "OK scanner returned ret " << ret << " for len :" << strlen(buffer) << endl;
+      }
+      
+      if (iworld.scannerstatus(ret,buffer)!=0)        {
+        cerr << "ERROR: status returned ret " << ret << " for \"" << buffer << "\""<< endl;
+        //          exit(233);
+        return -1;
+      }
+    }
+  return 0;
+}
+
